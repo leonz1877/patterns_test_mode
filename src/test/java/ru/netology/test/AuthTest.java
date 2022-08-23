04d88c8c3f8b1d45b79a88a7bd70b068ad3f6d55
Leonid Zhdanov <zhdanov.leon@gmail.com>
2022-08-23 23:51:11 +0400

    import com.codeborne.selenide.Configuration

--- src/test/java/ru/netology/test/AuthTest.java
+++ src/test/java/ru/netology/test/AuthTest.java
@@ -1,6 +1,7 @@
 package ru.netology.test;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
