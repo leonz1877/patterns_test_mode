708dadfeab02c921036e8e013a0b035aa4157c79
Leonid Zhdanov <zhdanov.leon@gmail.com>
2022-08-23 23:43:05 +0400

    'Configuration.headless' off

--- src/test/java/ru/netology/test/AuthTest.java
+++ src/test/java/ru/netology/test/AuthTest.java
@@ -1,7 +1,6 @@
 package ru.netology.test;
 
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,7 +17,7 @@ class AuthTest {
 
     @BeforeEach
     void setup() {
-        Configuration.headless = true;
+        //Configuration.headless = true;
         open("http://localhost:9999");
     }
 
